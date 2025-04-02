5a9e32f32711df32eb1baada34ec5aa289373f25
Soundaranarayanan <2k21eee65@kiot.ac.in>
2025-04-02 10:37:36 +0530

    02-04-2025 commit

--- CucumberDemo/src/test/java/runner/runnerTestNg.java
+++ CucumberDemo/src/test/java/runner/runnerTestNg.java
@@ -3,7 +3,7 @@ package runner;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-	@CucumberOptions(plugin={"pretty","html:target/cucumber-reports/Cucumber2.html"},
+	@CucumberOptions(plugin={"pretty","json:target/cucumber-reports/Cucumber4.json"},
 			features="src/test/resources/com/features/Login.feature",
 			glue="StrepDef")
 public class runnerTestNg extends AbstractTestNGCucumberTests{
